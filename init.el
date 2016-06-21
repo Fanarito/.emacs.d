32dd729fc2b12ac771ef56861640a19c88ab4b3c
Viktor Sævarsson <viktor.saevars@gmail.com>
2016-06-21 16:36:43 +0000

    Update init.el

--- init.el
+++ init.el
@@ -207,8 +207,10 @@
 
 ;; Helm keybindings
 (global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x b") 'helm-buffers-list)
+(global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-,") 'helm-imenu-anywhere)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
 
 
 ;; File associatons
